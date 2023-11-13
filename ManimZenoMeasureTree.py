3f179b0dcbaec563a3796b11a93bd133225197e5
Fernando Hurtado <mrfe333@gmail.com>
2023-11-12 22:32:43 -0600

    works now p much done

--- ManimZenoMeasureTree.py
+++ ManimZenoMeasureTree.py
@@ -21,7 +21,7 @@ class ManimMeasureTree(Scene):
         # Function to create division marks with decreasing size more aggressively
         def create_division_marks(points, depth):
             marks = []
-            length_factor = max(0.5, 3 - (depth * 0.5))
+            length_factor = max(0.2, 3 - (depth * 0.5))
             width_factor = max(2, 4 - (depth * 0.5))
             for point in points:
                 mark = Line(
@@ -84,8 +84,61 @@ class ManimMeasureTree(Scene):
 
             # Animate division marks and tree nodes and edges simultaneously
             animations = [Create(mark) for mark in marks] + [Create(node) for node in nodes] + [Create(edge) for edge in edges]
+
+            # Add step number and the number of parts at each step
+            step_number = MathTex(f"{i}", color=WHITE).scale(0.7)
+            parts_number = MathTex(r"2^{" + f"{i}" + r"}", color=WHITE).scale(0.7)
+            
+            # Position step number and parts number to the left of the binary tree, at different horizontal positions
+            step_number.move_to(np.array([-6.5, root_y - (i - 1) * vertical_spacing - 0.5, 0]))
+            parts_number.move_to(np.array([-6, root_y - (i - 1) * vertical_spacing - 0.5, 0]))  # Align with step_number
+
+            # Include step number and parts number in the animations
+            animations.extend([Write(step_number), Write(parts_number)])
+            
             self.play(*animations, run_time=animation_speed)
 
             self.wait(1)  # Consistent wait time between levels
 
-        self.wait(5)
+
+        # Create the set representation with large brackets and many small points
+        set_width = 10  # Width of the set representation
+        set_bracket_left = MathTex(r"\{").scale(2)  # Correct the brackets for sets
+        set_bracket_right = MathTex(r"\}").scale(2)
+        # This will create a line of small dots between the brackets to represent many points
+        # Use MathTex and ensure the dots are in math mode
+        set_dots = MathTex(r"\cdot " * 100).scale(0.4)
+
+        # Position the set brackets at the center horizontally and shift them down
+        center_point = n_line.get_center()  # Current center point of the number line
+        set_bracket_left.move_to(center_point + DOWN*6 + LEFT*(set_width/2))
+        set_bracket_right.move_to(center_point + DOWN*6 + RIGHT*(set_width/2))
+        # Position the dots at the center of the set brackets
+        set_dots.move_to(center_point + DOWN*6)
+        
+        # Create the omega symbol and the "2^ℕ" notation
+        omega_symbol = MathTex(r"\omega").scale(0.7)
+        two_to_the_n_symbol = MathTex(r"2^{\mathbb{N}}").scale(0.7)
+
+        # Position the omega symbol and the "2^ℕ" notation at the specified horizontal locations
+        # and align them vertically with the set brackets which are at DOWN*3
+        omega_symbol.move_to(np.array([-6.5, set_bracket_left.get_y(), 0]))
+        two_to_the_n_symbol.move_to(np.array([-6, set_bracket_right.get_y(), 0]))
+
+        # Create the ellipsis that indicates the continuation of the process below the binary tree
+        continuation_dots = MathTex(r"\cdots").next_to(tree, DOWN, buff=0.5)
+
+        # Create the continuation dots that will appear below the step number and part numbers
+        left_continuation_dots = MathTex(r"\cdots")
+        continuation_dots_y = continuation_dots.get_center()[1]
+        left_continuation_dots.move_to(np.array([-6.25, continuation_dots_y, 0]))  # Centered between -6 and -6.5
+        
+
+        # Animate the creation of the continuation dots
+        self.play(Write(continuation_dots), Write(left_continuation_dots))
+
+        # Now add the animations for the set brackets, dots, omega symbol, and "2^ℕ" notation
+        self.play(Write(set_bracket_left), Write(set_bracket_right), Write(set_dots),
+                  Write(omega_symbol), Write(two_to_the_n_symbol))
+
+        self.wait(10)
